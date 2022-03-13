58993a5670e745aa7c30b6ec4ef4700d135b5fad
Studentfaruk0308 <studentfaruk0308@gmail.com>
2022-03-13 23:32:00 +1100

    on 13 Mar 2022

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(x){
+    cats.push(x);
+}
+
+function destructivelyPrependCat(x){
+    cats.unshift(x);
+}
+
+
+function destructivelyRemoveLastCat(){
+   cats.pop();
+}
+
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    }
+    
+
+function appendCat(x){
+   let result = [...cats, x];
+   return result;
+}
+
+
+function prependCat(x){
+    let result = [x,...cats];
+    return result;
+   }
+
+function removeLastCat(x){
+    return cats.slice(0,-1);
+}
+
+function removeFirstCat(x){
+return cats.slice(1);
+}
